98e64a0a6840fa027a1b91969d9c3d051394bd6e
randomuser191 <88069151+randomuser191@users.noreply.github.com>
2022-09-19 09:55:05 -0500

    Added toString()

--- M359/Unit3/src/U3/Examples/Pizza.java
+++ M359/Unit3/src/U3/Examples/Pizza.java
@@ -21,6 +21,27 @@ public class Pizza {
         isDeepDish = d;
     }
 
+    /**
+     * toString method always has toString()
+     * creates string and returns it
+     * prints object
+     * @return string
+     */
+    public String toString(){
+        String str = "";
+        str += "My pizza is from " + store + ". It is " + inches + " inches. \n";
+        if(isDeepDish){
+            str+= "I love deep dish.";
+        }else{
+            str += "Thin crust is the way to go.";
+        }
+        if(numToppings == 0){
+            str+= "\nI love cheese pizza.";
+        }else{
+            str+= "\nMy pizza has " + numToppings + " toppings. \n";
+        }
+        return str;
+    }
     public String getStore() {
         return store;
     }
--- M359/Unit3/src/U3/Examples/PizzaTester.java
+++ M359/Unit3/src/U3/Examples/PizzaTester.java
@@ -3,20 +3,20 @@ package U3.Examples;
 public class PizzaTester {
     public static void main(String[] args) {
         Pizza cheese = new Pizza("Dominoes", 6, false);
-        Pizza pepperoni = new Pizza("Pizza Hut", 6, 5, true);
+        Pizza pepperoni = new Pizza("Pizza Hut", 3, 5, true);
 
-        System.out.println(cheese.getNumToppings());
-        System.out.println(cheese.isDeepDish());
-        System.out.println(pepperoni.getNumToppings());
-        System.out.println(pepperoni.getStore());
-        System.out.println('test edit');
-
-        cheese.setStore("Pizza Hut");
-        cheese.setInches(3);
-        cheese.setDeepDish(true);
-        cheese.setNumToppings(3);
-        System.out.println(cheese.getInches());
-        System.out.println(cheese.isDeepDish());
-        System.out.println(cheese.getNumToppings());
+//        System.out.println(cheese.getNumToppings());
+//        System.out.println(cheese.isDeepDish());
+//        System.out.println(pepperoni.getNumToppings());
+//        System.out.println(pepperoni.getStore());
+        System.out.println(pepperoni);
+        System.out.println(cheese);
+//        cheese.setStore("Pizza Hut");
+//        cheese.setInches(3);
+//        cheese.setDeepDish(true);
+//        cheese.setNumToppings(3);
+//        System.out.println(cheese.getInches());
+//        System.out.println(cheese.isDeepDish());
+//        System.out.println(cheese.getNumToppings());
     }
 }
