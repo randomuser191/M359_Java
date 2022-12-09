8810a192329691e9c22732a72c14d1e3ffe90568
randomuser191 <88069151+randomuser191@users.noreply.github.com>
2022-12-09 10:09:10 -0600

    Trivia Game
    Trivia Game

--- M359/Unit6/src/Arrays/Work/TriviaDriver.java
+++ M359/Unit6/src/Arrays/Work/TriviaDriver.java
@@ -5,27 +5,48 @@ public class TriviaDriver {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner inp = new Scanner(System.in);
         TriviaGame g = new TriviaGame();
+        int streak = 0;
         g.getData();
         Question[] q = g.getQuestions();
-        for(int i = 0; i < q.length; i++){
-            System.out.println(q[i]);
+        String indexes = "";
+
+        System.out.println("\nWelcome to Akshat's Trivia. What is your name?");
+        String name = inp.nextLine();
+        System.out.println("Ok " + name + ". Let's begin!\n");
+
+        for(int i = 14; i < q.length; i++){
+            int idx = (int)(Math.random() * q.length);
+            if(indexes.contains("" + idx)) idx = (int)(Math.random() * q.length);
+            indexes += "" + idx;
+            System.out.println(q[idx]);
             System.out.println("Type A, B, C, or D below");
             String userC = inp.next();
-            if(g.checkAnswer(userC.toUpperCase(), q[i])){
-                System.out.println("Correct");
+            if(g.checkAnswer(userC.toUpperCase(), q[idx])){
+                System.out.println("\nCorrect!");
+                g.setPoints(g.getPoints() + q[idx].getPoints());
+                g.setqCor(g.getqCor() + 1);
+                streak++;
             }else{
-                System.out.println("Incorrect");
-            };
-            System.out.println("Would you like to continue? Y or N");
-            userC = inp.next();
+                System.out.println("\nIncorrect. The correct answer was " + q[idx].getAnswer());
+                streak = 0;
+            }
+            g.setqAns(g.getqAns() + 1);
+            g.setStreak(streak);
+            System.out.println("\nWould you like to continue? Y or N");
+            userC = inp.next().toLowerCase();
             if(userC.equals("y")){
-                i++;
+                System.out.println("\n\n" + g.getStats(false));
             }else if(userC.equals("n")){
+                System.out.println("\n\n" + g.getStats(true));
                 break;
             }else{
                 System.out.println("Enter Y or N");
                 userC = inp.next();
             }
+
+
         }
+        System.out.println("\n Congratulations " + name + "! You completed the Trivia. Here's how you did. \n");
+        System.out.println("\n" + g.getStats(true));
     }
 }
--- M359/Unit6/src/Arrays/Work/TriviaGame.java
+++ M359/Unit6/src/Arrays/Work/TriviaGame.java
@@ -5,11 +5,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriviaGame {
-        private int streak = 0;
-        private int points = 0;
+        private int streak;
+        private int points;
+        private int qAns;
+        private int qCor;
         private Question[] questions;
         public TriviaGame(){
             questions = new Question[16];
+            streak = 0;
+            points = 0;
+            qAns = 0;
+            qCor = 0;
         }
         public void getData() throws FileNotFoundException {
             File qFile = new File("Unit6/src/Arrays/Work/TriviaQuestions.txt");
@@ -27,9 +33,21 @@ public class TriviaGame {
                 questions[i] = q;
             }
         }
-    public boolean checkAnswer(String ans, Question q){
-        return (ans.equals(q.getAnswer()));
-    }
+        public boolean checkAnswer(String ans, Question q){
+            return (ans.equals(q.getAnswer()));
+        }
+        public String getStats(boolean end){
+            String str;
+            if(!end) str = "**** Current Stats **** \n";
+            else str = "**** Final Stats **** \n";
+
+            str += "Answer Streak: " + streak + "\n";
+            str += "Total Points: " + points + "\n";
+            str += "Questions Answered: " + qAns + "\n";
+            str += "Questions Answered Correctly: " + qCor + "\n";
+            if(end) str+= "Percent Correct: " + ((double)(qCor)/qAns) * 100 + "%\n";
+            return str;
+        }
 
     public int getStreak() {
         return streak;
@@ -53,5 +71,21 @@ public class TriviaGame {
 
     public void setQuestions(Question[] questions) {
         this.questions = questions;
+    }
+
+    public int getqAns() {
+        return qAns;
+    }
+
+    public void setqAns(int qAns) {
+        this.qAns = qAns;
+    }
+
+    public int getqCor() {
+        return qCor;
+    }
+
+    public void setqCor(int qCor) {
+        this.qCor = qCor;
     }
 }
